abe397b99bc266cd9d5b3770da3d201ada232ef4
Gerald Bauer <gerald.bauer@gmail.com>
2013-05-10 23:05:59 +0200

    rm pkg

--- pkg/beerdb-labels-0.0.1/History.md
+++
@@ -1,3 +0,0 @@
-### 0.1.0 / 2013-05-11
-
-* Everything is new. First release
--- pkg/beerdb-labels-0.0.1/lib/beerdb/labels/version.rb
+++
@@ -1,6 +0,0 @@
-
-module BeerDb ; end    # forward reference (for hoe require)
-
-module BeerDb::Labels
-  VERSION = '0.0.1'
-end
